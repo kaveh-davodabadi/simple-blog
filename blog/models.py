fe766645ae66494530d7270e911b88393758aafd
kaveh-davodabadi <kavehdavodabadi@gmail.com>
2025-08-12 20:13:31 +0330

    completed models, file settings

---
+++ blog/models.py
@@ -0,0 +1,66 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    image = models.ImageField(upload_to="images/")
+    related_url = models.URLField(max_length=200, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=False)
+    published_at = models.DateTimeField(blank=True, null=True)
+    slug = models.SlugField(unique=True, max_length=200)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    email = models.EmailField(max_length=100)
+    full_name = models.CharField(max_length=70)
+    content = models.TextField()
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    is_approved = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment by {self.full_name}"
+
+
+class Reply(models.Model):
+    comment = models.OneToOneField(
+        Comment, on_delete=models.CASCADE, related_name="reply"
+    )
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Reply to {self.comment.full_name}"
+
+
+class Document(models.Model):
+    title = models.CharField(max_length=100)
+    file = models.FileField(upload_to="documents/")
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="documents")
+
+    def __str__(self):
+        return f"{self.title} ({self.post.title})"
+
+
+class PortfolioProject(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    image = models.ImageField(upload_to="uploads/images/")
+    project_url = models.URLField(max_length=200, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=False)
+    published_at = models.DateTimeField(blank=True, null=True)
+    slug = models.SlugField(unique=True, max_length=200)
+
+    def __str__(self):
+        return self.title
